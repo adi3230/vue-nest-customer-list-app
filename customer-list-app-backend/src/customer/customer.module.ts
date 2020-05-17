9219dc8884d4fca7e976f95abcf9b90d999a6d16
Aditya Joshi <aditya@lillydoo.com>
2020-05-17 19:03:11 +0200

    feat(be): setup endpoint for API for FE to consume

--- customer-list-app-backend/src/customer/customer.module.ts
+++ customer-list-app-backend/src/customer/customer.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CustomerController } from "./customer.controller";
+import { CustomerService } from "./customer.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CustomerSchema } from "./schemas/customer.schema";
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([{
+            name: 'Customer',
+            schema: CustomerSchema,
+        }])
+    ],
+    controllers: [CustomerController],
+    providers: [CustomerService]
+})
 export class CustomerModule {}
